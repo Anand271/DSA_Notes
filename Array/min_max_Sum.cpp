35aa1e5fbc1c2f605b4fd1be417834908e0de276
Santosh Kumar Anand <anandsa271@gmail.com>
2022-11-12 00:25:12 +0530

    12 Nov

--- Array/min_max_Sum.cpp
+++ Array/min_max_Sum.cpp
@@ -1,3 +1,13 @@
+/*
+Given an array A of size N of integers. Your task is to find the sum of minimum and maximum element in the array.
+
+Input:
+N = 5
+A[] = {-2, 1, -4, 5, 3}
+Output: 1
+Explanation: min = -4, max =  5. Sum = -4 + 5 = 1
+*/
+
 #include <iostream>
 #include<math.h>
 
